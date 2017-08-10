44c0d29e0a0bb8fccde2666c4d39694a1c13610f
Robin <robin_acet@yahoo.com>
2017-08-10 19:08:17 +0530

    Changes10/08/2017

--- eXtend/HybridFramework/Executor/GetLatestExcelvalue.java
+++ eXtend/HybridFramework/Executor/GetLatestExcelvalue.java
@@ -1,5 +1,12 @@
 package Executor;
 
 public class GetLatestExcelvalue {
+	
+	public void getLatestValue(String testCase, String stepNumber,String testStep, String keyword, String Locator, String TestData1,String TestData2,String TestData3,String TestData4,String output)
+	{
+		
+		
+		
+	}
 
 }
